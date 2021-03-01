5b4ccb0d5dba28fa059331d81372b1b453b1228b
starry69 <starry369126@outlook.com>
2021-03-01 17:17:30 +0530

    even satan used to be an angel
    Signed-off-by: starry69 <starry369126@outlook.com>

---
+++ patterns/pattern4.c
@@ -0,0 +1,17 @@
+/* prints: ****
+           ***
+           **
+           *
+*/
+
+#include<stdio.h>
+
+void main() {
+   int i,j;
+   for(i=1;i<=4;i++) {
+       for(j=1;j<=(5-i);j++) {
+           printf("*");
+       }
+       printf("\n");
+   }
+}
---
+++ patterns/pattern5.c
@@ -0,0 +1,20 @@
+/* prints: ****
+            ***
+             **
+              *
+*/
+
+#include<stdio.h>
+
+void main() {
+   int i,j,k;
+   for(i=1;i<=4;i++) {
+       for(j=1;j<=(i-1);j++) {
+           printf(" ");
+       }
+       for(k=1;k<=(5-i);k++) {
+          printf("*");
+       }
+       printf("\n");
+   }
+}
